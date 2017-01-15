250e402fceab550abaaa83687e2cf72f4d907ed9
elton <el10savio96@gmail.com>
2017-01-15 18:55:28 +0530

    Commit

---
+++ classifier.py
@@ -0,0 +1,25 @@
+from PIL import Image
+import numpy, os
+from sklearn.neighbors import KNeighborsClassifier
+
+path="/home/elton/Dropbox/Programs/Python/ML/MNIST/notMNIST_small"
+Xlist=[]
+Ylist=[]
+
+for directory in os.listdir(path):
+	for file in os.listdir(path+"/"+directory):
+		img=Image.open(path+"/"+directory+"/"+file)
+		featurevector=numpy.array(img).flatten()
+		Xlist.append(featurevector)
+		Ylist.append(directory)
+
+Xtest=Xlist[:20]
+Ytest=Ylist[:20]
+
+knn = KNeighborsClassifier(n_neighbors=1)
+print('KNN score: %f' % (knn.fit(Xlist, Ylist).score(Xtest, Ytest)))
+
+path_test=raw_input("Path:")
+
+print knn.predict(numpy.array(Image.open(path_test)).flatten())
+
